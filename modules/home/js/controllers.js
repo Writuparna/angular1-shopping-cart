6b7929a6aec4c9b20900a0390ab2b01c9b4bb56d
Writuparna <writu.chwdhry@gmail.com>
2017-07-07 13:47:02 +0530

    product sorting

--- modules/home/js/controllers.js
+++ modules/home/js/controllers.js
@@ -3,17 +3,37 @@
 angular.module('cartApp.home.controller', []).controller('HomeController', ['$scope','productsFactory',function($scope,productsFactory){
 
 	$scope.cartAry = [];
+	$scope.sameCatgAry = [];
 
 	$scope.cartAryFn = function(){
 
 		productsFactory.productGetFn()
-			.then(function(productGetArray){
-				$scope.cartAry = productGetArray;
-				$scope.cartAryPro = productGetArray.productsInCart;
-			},function(){
+			.then(function(allproducts){
+				$scope.allproductsScope = allproducts;
+				sortCategory($scope.allproductsScope);
+				},function(){
 				console.log('data cannot retrieved');
 			});
 	}
 	$scope.cartAryFn();
+	function sortCategory(cartAry){		
+		$scope.cartAryScope = cartAry.productsInCart;
+		for(var i=0; i<$scope.cartAryScope.length; i++){
+			$scope.sameCatgAry.push($scope.cartAryScope[i]);
+			var count = 0;
+			for(var j=0; j<$scope.sameCatgAry.length; j++){
+				if($scope.cartAryScope[i].p_catg_id==$scope.sameCatgAry[j].p_catg_id){
+					count++;
+					console.log("Count: "+count);
+				}
+			}
+			if(count>1){
+				$scope.sameCatgAry.pop;
+			}
+		}
+			console.log(JSON.stringify($scope.sameCatgAry));
+		//$scope.cartAryPro = productGetArray.productsInCart;
+	
+	}
 
 }]);
