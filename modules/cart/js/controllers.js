8b29a1b5ff502dc7dc3216ff15dec550900b8097
Writuparna <writu.chwdhry@gmail.com>
2017-07-09 13:21:22 +0530

    total price update when quantity change

--- modules/cart/js/controllers.js
+++ modules/cart/js/controllers.js
@@ -3,10 +3,29 @@
 angular.module('cartApp.cart.controller', []).controller('CartController', ['$scope','productsFactory',function($scope,productsFactory){
 
 	$scope.cartAry = productsFactory.getCartObjFn();
-
-	$scope.newQty = "";
-
 	console.log('cart page: '+ JSON.stringify($scope.cartAry));
+	console.log('new Qty: '+ $scope.newQty);
 
+	$scope.changeQtyFn = function(newQtyParam,$index){
+		$scope.newQtyScope = newQtyParam;
+		console.log('new qty: '+ $scope.newQtyScope + ' index price ' + JSON.stringify($scope.cartAry[$index].proPrice));
+		$scope.totalPrice = $scope.newQtyScope * ($scope.cartAry[$index].proPrice);
+	}
+
+	for (var i = 0; i < $scope.cartAry.length; i++) {
+		$scope.newQtyScope = $scope.cartAry[i].proQty;
+	}
+	/**/
+	/*$scope.$watch('cartAry',function(newValue,oldValue){
+		for (var i = 0; i < $scope.cartAry.length; i++) {
+			if(newValue[i].proPrice!=oldValue[i].proPrice){
+				console.log('new value: '+ JSON.stringify(newValue));
+			}
+		}
+	});*/
+
+
+
+			
 
 }]);
