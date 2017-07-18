43009a698057b97e2f52aff199dcecb2854e9e22
Writuparna <writu.chwdhry@gmail.com>
2017-07-18 09:02:20 +0530

    login logout working

--- modules/cart/js/controllers.js
+++ modules/cart/js/controllers.js
@@ -1,11 +1,8 @@
 'use strict';
 
-angular.module('cartApp.cart.controller', []).controller('CartController', ['$scope','productsFactory',function($scope,productsFactory){
+angular.module('cartApp.cart.controller', []).controller('CartController', ['$scope','productsFactory','$state',function($scope,productsFactory,$state){
 
 	$scope.cartAry = productsFactory.getCartObjFn();
- 	//console.log('cart page: '+ JSON.stringify($scope.cartAry));
-		
-	//console.log('whole array: '+ JSON.stringify($scope.cartAry));
 	$scope.grandTotal = 0;
 	for (var i = 0; i < $scope.cartAry.length; i++) {
 		$scope.grandTotal = $scope.grandTotal + $scope.cartAry[i].totalPrice;
@@ -14,11 +11,9 @@ angular.module('cartApp.cart.controller', []).controller('CartController', ['$sc
 	$scope.changeQtyFn = function(newQtyParam,$index){
 		$scope.newQtyScope = newQtyParam;
 		if($scope.newQtyScope > 0 && $scope.newQtyScope!="" ){
-			//console.log('new qty: '+ $scope.newQtyScope + ' index price ' + JSON.stringify($scope.cartAry[$index].proPrice));
 			$scope.cartAry[$index].totalPrice = $scope.newQtyScope * ($scope.cartAry[$index].proPrice);
 			$scope.grandTotal=0;
 			for (var i = 0; i < $scope.cartAry.length; i++) {
-				//console.log('selected array total price: '+ $scope.cartAry[i].totalPrice);
 				$scope.grandTotal = $scope.grandTotal + $scope.cartAry[i].totalPrice;
 			}
 		}
@@ -29,23 +24,20 @@ $scope.removeItem = function($index){
 	$scope.grandTotal=0;
 
 	for (var i = 0; i < $scope.cartAry.length; i++) {
-		//console.log('selected array total price: '+ $scope.cartAry[i].totalPrice);
 		$scope.grandTotal = $scope.grandTotal + $scope.cartAry[i].totalPrice;
-		//console.log('old grand total: '+$scope.grandTotal);
 	}
 	productsFactory.removeCartItemFn();
-	//console.log('array after remove item: '+ JSON.stringify($scope.cartAry));
-		console.log('selectedProAry: '+ JSON.stringify(productsFactory.selectedProAry));
+	console.log('selectedProAry: '+ JSON.stringify(productsFactory.selectedProAry));
 
 }
  $scope.cartToWishList = function($index){
 	var itemMovedToWishlist = productsFactory.selectedProAry[$index];
-	//productsFactory.cartToWishlistFn(itemMovedToWishlist);
 	console.log(itemMovedToWishlist);
 }
 
 $scope.checkOut =function(){
-	
+	console.log('abc');
+	$state.go('login');
 }
 
 			
